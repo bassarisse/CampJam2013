ca54ba2708b48eca63046c2323484ca9c9c6d5d9
Bruno Assarisse <bruno@assarisse.com.br>
2013-07-31 23:58:54 -0300

    Filled circle function URL

--- Classes/Entities/Shadow.cpp
+++ Classes/Entities/Shadow.cpp
@@ -26,6 +26,7 @@ void Shadow::draw() {
                  0, 200, false, 1, 0.15f);
     
 	ccDrawColor4B(0, 0, 0, 40);
+    // https://github.com/cocos2d/cocos2d-x/issues/2287
 	ccDrawFilledCircle(
                        ccp(entityPosition.x, entityPosition.y - (entitySize.height / 2.4f)),
                        entitySize.width / 4.5f,
